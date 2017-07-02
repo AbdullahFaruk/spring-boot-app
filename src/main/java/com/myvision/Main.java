854ae865f99be84487c024e143b78b0b819bb43c
aalfaruk <abdullah.faruk85@gmail.com>
2017-07-02 18:30:37 -0400

    first and initial commit for microservices

---
+++ src/main/java/com/myvision/Main.java
@@ -0,0 +1,16 @@
+package com.myvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by shawon on 7/1/17.
+ */
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
+    }
+}
